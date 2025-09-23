c3ab2711f8f0f9f93b35f59b91812589b29e4f3f
coolboymalique17 <maliquefitzcharles@gmail.com>
2025-09-23 14:38:58 -0400

    1.1

--- Project_SpinShot/Assets/Scripts/PlayerMovement.cs
+++ Project_SpinShot/Assets/Scripts/PlayerMovement.cs
@@ -3,16 +3,16 @@ using UnityEngine.InputSystem;
 
 public class Test : MonoBehaviour
 {
-    public InputAction movePlayer;
+    public InputAction[] movePlayer;
     public float speed = 5f;
-    private Vector2 movementInput;
+    public Vector2 movementInput;
     private Rigidbody2D rb;
-
+    public int playerNumber;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-       
-        movePlayer.Enable();
+
+        movePlayer[playerNumber].Enable();
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,7 +24,8 @@ public class Test : MonoBehaviour
     void Update()
     {
   
-        movementInput = movePlayer.ReadValue<Vector2>();
+        movementInput = movePlayer[playerNumber].ReadValue<Vector2>();
        transform.Translate(movementInput.y * speed * Time.deltaTime * Vector3.right);
+        Debug.Log(movementInput.y);
     }
 }
