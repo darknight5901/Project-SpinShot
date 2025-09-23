fb441efd0e36b2fa809c4139e892e3fd6e62b1be
coolboymalique17 <maliquefitzcharles@gmail.com>
2025-09-23 13:43:56 -0400

    playersMade

--- Project_SpinShot/Assets/Scripts/NewMonoBehaviourScript.cs
+++ Project_SpinShot/Assets/Scripts/NewMonoBehaviourScript.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class NewMonoBehaviourScript : MonoBehaviour
 {
-    public int frameCount;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,7 +11,6 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        frameCount++;
-        Debug.Log(frameCount);
+        
     }
 }
