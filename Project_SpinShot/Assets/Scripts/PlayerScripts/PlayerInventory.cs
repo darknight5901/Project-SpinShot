a0ece80543ac3a902d8a87bf12bf3e9f52f748e7
coolboymalique17 <maliquefitzcharles@gmail.com>
2025-11-30 22:03:55 -0500

    StatsManagerCreated

--- Project_SpinShot/Assets/Scripts/PlayerScripts/PlayerInventory.cs
+++ Project_SpinShot/Assets/Scripts/PlayerScripts/PlayerInventory.cs
@@ -1,20 +1,20 @@
-using NUnit.Framework;
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
+    public float currency;
     public int inventorySize = 6;
     public List<BaseItem> iContent = new List<BaseItem>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void AddItemToList(BaseItem item)
     {
@@ -23,7 +23,7 @@ public class PlayerInventory : MonoBehaviour
             iContent.Add(item);
             Debug.Log($"Item {item} was added, the new inventory size is {iContent.Count}");
         }
-        else 
+        else
         {
             Debug.Log($"The list is full. Cannot add {item}. The max size is {inventorySize}");
         }
---
+++ Project_SpinShot/Assets/Scripts/PlayerScripts/StatsManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class StatsManager : MonoBehaviour
+{
+
+    [Header("Player Movement")]
+    public float speed = 5f;
+    public bool isAI;
+    public float shotPower = 1.0f;
+    public float shotPowerMulti = 10.0f;
+    [Header("Player Inventory")]
+
+
+
+    public PlayerInventory playerInventory;
+
+}
